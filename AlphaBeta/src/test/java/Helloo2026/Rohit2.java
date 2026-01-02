506203759fe677440d5e3b124bad5b66c2686245
Rohit321774 <rohit.feb2001@gmail.com>
2026-01-02 11:52:33 +0530

    Delete AlphaBeta directory

--- AlphaBeta/src/test/java/Helloo2026/Rohit2.java
+++
@@ -1,9 +0,0 @@
-package Helloo2026;
-
-public class Rohit2 {
-	public static void main(String[] args) {
-		System.out.println("How Are You");
-		System.out.println("I am fine Are You");
-	}
-
-}
--- AlphaBeta/src/test/java/Helloo2026/RohitDemo.java
+++
@@ -1,8 +0,0 @@
-package Helloo2026;
-
-public class RohitDemo {
-	public static void main(String[] args) {
-		System.out.println("OKKK Are You");
-	}
-
-}
